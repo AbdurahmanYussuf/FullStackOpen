6450fe1f26a2706e5747de7278a816076251c5dc
AbdurahmanYussuf <abdi_arshavin@live.ca>
2021-05-09 23:40:20 -0400

    seprate modules

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const Header = ({ course }) => {
+  return (
+    <h1>Web development curriculum</h1>
+  )
+}
+
+const Total = ({ course }) => {
+  const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
+  return(
+    <p><strong>Total of exercises {total}</strong></p>
+  ) 
+}
+
+const Part = ({part}) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>    
+  )
+}
+
+const Content = ({course}) => {
+  return (
+    <>
+      <h3>{course.name}</h3>
+      <div>
+        {course.parts.map(part => <Part key={part.id} part={part} />)}
+      </div>
+      <Total course={course} />
+    </>
+  )
+}
+
+const Course = ({course}) => {
+  return(
+    <>
+      <Header course={course} />
+      <Content course={course[0]} />
+      <Content course={course[1]} />
+    </>
+  )
+}
+
+export default Course
